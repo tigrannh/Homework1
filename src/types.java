c90e3439ba3ce8774d36f4791649d967cd5749d3
Tigran <tigo.hakobyan.l@gmail.com>
2021-03-29 01:57:33 +0400

    Initial commit

---
+++ src/types.java
@@ -0,0 +1,21 @@
+public class types {
+    public static void main(String[] args) {
+        byte a = -24;
+        short b = 11000;
+        int c = -22000;
+        long d = 100000000L;
+        float e = 23.45f;
+        double f = 134.66;
+        boolean g = true;
+        char h = '*';
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+    }
+}
